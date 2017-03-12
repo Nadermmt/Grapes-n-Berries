ba251e2a4a2d33ce57d1a8eab96ec5d2b9ec8aff
Nadermmt <nadermmt@gmail.com>
2017-03-12 08:38:41 +0200

    API declaration.

---
+++ app/src/main/java/com/example/nader/grapesnberries/gnbAPI.java
@@ -0,0 +1,12 @@
+package com.example.nader.grapesnberries;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface gnbAPI {
+    @GET("products?count=10")
+    Call<List<Product>> content(@Query("from") int id);
+}
